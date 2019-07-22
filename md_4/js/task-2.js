91eebd5080a0ba60284d54da4bf0ab0bd7cb5e13
Чудовий Світ <homenkosasha13@gmail.com>
2019-07-22 18:21:08 +0300

    md 04

---
+++ md_4/js/task-2.js
@@ -0,0 +1,24 @@
+const inventory = {
+  items: ['Монорельса', 'Фильтр'],
+  add(itemName) {
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryOperation = function(itemName, inventoryAction) {
+  console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
+  inventoryAction(itemName);
+};
+
+invokeInventoryOperation('Аптечка', inventory.add.bind(inventory));
+// Invoking add opeartion on Аптечка
+
+console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+invokeInventoryOperation('Фильтр', inventory.remove.bind(inventory));
+// Invoking remove opeartion on Фильтр
+
+console.log(inventory.items); // ['Монорельса', 'Аптечка']
